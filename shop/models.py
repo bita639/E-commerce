079d95316e119f31b8c848d88798c493f09fd89d
bita639 <01625326156dj@gmail.com>
2021-01-12 22:45:11 +0600

    static files and templates are uploaded

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True, verbose_name='name')
+    slug = models.SlugField(max_length=200, db_index=True, unique=True, verbose_name='slug')
+
+    #class meta???????
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', verbose_name='category')
+    name = models.CharField(max_length=200, db_index=True, verbose_name='name')
+    slug = models.SlugField(max_length=200, db_index=True, verbose_name='slug')
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True, verbose_name='description')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='price')
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='updated at')
+
+    class Meta:
+        ordering = ["-created_at"]
+
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
